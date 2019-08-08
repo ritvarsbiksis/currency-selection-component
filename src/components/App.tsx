155684ec1fe3cb35222cd556bc3e0ee9f0e63db5
Ritvars Biksis <ritvars.biksis@tieto.com>
2019-08-08 21:06:21 +0300

    :lipstick: Update main UI styles. Material UI components add.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,30 @@
 import React, { Component } from 'react'
+import { Paper, Typography } from '@material-ui/core'
+import { Theme, WithStyles, createStyles, withStyles } from '@material-ui/core/styles'
 
-export class App extends Component {
+const stylesApp = (theme: Theme) =>
+  createStyles({
+    root: {
+      width: '400px',
+      padding: theme.spacing(2),
+      position: 'absolute',
+      left: 'calc(50% - 216px)',
+      top: 'calc(50% - 120px)'
+    }
+  })
+
+interface Props extends WithStyles<typeof stylesApp> {}
+
+class _App extends Component<Props> {
   render () {
-    return <div>currency-selection-component</div>
+    const { classes } = this.props
+
+    return (
+      <Paper {...{ className: classes.root }}>
+        <Typography {...{ component: 'p' }}>Place for currency buttons...</Typography>
+      </Paper>
+    )
   }
 }
+
+export const App = withStyles(stylesApp)(_App)
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 import { App } from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.querySelector('#root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
